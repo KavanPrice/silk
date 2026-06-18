6bc7c4cf0cf3d096bbe698d756a2fc0406f2d078
Kavan Price <kavanprice@gmail.com>
2026-06-18 22:56:10 +0100

    Add fixed length header to packet definition

--- lib/packet.ml
+++ lib/packet.ml
@@ -1,6 +1,13 @@
-type packet = {
+type fixed_header = {
   packet_type : Packet_type.packet_type;
-  fixed_header : string;
-  variable_header : string;
-  payload : string;
+  flags : Flags.fixed_header_flags;
+}
+
+type variable_header = string
+type payload = string
+
+type packet = {
+  fixed_header : fixed_header;
+  variable_header : variable_header;
+  payload : payload;
 }
